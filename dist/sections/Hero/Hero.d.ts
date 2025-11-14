bb51ce617730cb8919957eeb1f859e9dcf4a7992
Diego Alba <diegoalba15@icloud.com>
2025-11-14 13:11:13 -0400

    Chore: modify Schema

--- dist/sections/Hero/Hero.d.ts
+++ dist/sections/Hero/Hero.d.ts
@@ -6,13 +6,36 @@ export declare const heroSettingsSchema: ({
     label: string;
     type: string;
     default: string;
+    info: string;
     options?: undefined;
+    customType?: undefined;
 } | {
     id: string;
     label: string;
     type: string;
-    default?: undefined;
+    default: string;
+    info?: undefined;
     options?: undefined;
+    customType?: undefined;
+} | {
+    id: string;
+    label: string;
+    type: string;
+    options: {
+        value: boolean;
+        label: string;
+    }[];
+    default: boolean;
+    info?: undefined;
+    customType?: undefined;
+} | {
+    id: string;
+    label: string;
+    type: string;
+    default: number;
+    info: string;
+    options?: undefined;
+    customType?: undefined;
 } | {
     id: string;
     label: string;
@@ -22,10 +45,46 @@ export declare const heroSettingsSchema: ({
         label: string;
     }[];
     default: string;
+    info?: undefined;
+    customType?: undefined;
+} | {
+    id: string;
+    label: string;
+    type: string;
+    customType: string;
+    default: string;
+    info?: undefined;
+    options?: undefined;
+} | {
+    id: string;
+    label: string;
+    type: string;
+    customType: string;
+    default?: undefined;
+    info?: undefined;
+    options?: undefined;
+} | {
+    id: string;
+    label: string;
+    type: string;
+    info: string;
+    default?: undefined;
+    options?: undefined;
+    customType?: undefined;
 } | {
     id: string;
     label: string;
     type: string;
     default: boolean;
+    info?: undefined;
+    options?: undefined;
+    customType?: undefined;
+} | {
+    id: string;
+    label: string;
+    type: string;
+    customType: string;
+    info: string;
+    default?: undefined;
     options?: undefined;
 })[];
