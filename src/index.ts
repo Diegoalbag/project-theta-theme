e09f832f596b369e2a2fcbc53e6a388f7bdf4b46
Diego Alba <diegoalba15@icloud.com>
2026-02-01 22:50:29 -0400

    Chore: fix build

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import "./index.css";
 export * from "./registry";
+
+const themeName = 'project-theta-theme'; // must match NEXT_PUBLIC_THEME_NAME
+(window as any).__THETA_THEMES__ = (window as any).__THETA_THEMES__ || {};
+(window as any).__THETA_THEMES__[themeName] = {
+  name: themeName,
+  version: '1.0.0',
+  sectionsComponents: { /* your sections */ },
+  sectionSettingsSchemas: { /* your schemas */ },
+};
